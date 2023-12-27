7b05f8aaaf6fc0d83a8e5b922a246a75e609915e
rishusingh022 <rishusingh022@gmail.com>
2023-12-27 22:14:57 +0530

    chore: clean codebase

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,39 +1,25 @@
-/* eslint-disable jsx-a11y/img-redundant-alt */
-import { Box, styled } from "@mui/material";
 import "../styles/Home.css";
-
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 import AuthForm from "../components/AuthForm";
 
-const Component = styled(Box)`
-  background: #ffffff;
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-`;
-
 export default function Home() {
   return (
-    <Component>
-      <Container>
-        <Row>
-          <Col xs={6} className="left-grid">
-            <img
-              class="title-image"
-              src={"/images/firsync-home.png"}
-              alt="Logo"
-            />
-          </Col>
-          <Col className="right-grid">
-            <AuthForm />
-          </Col>
-        </Row>
-      </Container>
-    </Component>
+    <Container>
+      <Row>
+        <Col className="left-grid">
+          <img
+            class="title-image"
+            src={"/images/firsync-home.png"}
+            alt="Logo"
+          />
+        </Col>
+        <Col className="right-grid">
+          <AuthForm />
+        </Col>
+      </Row>
+    </Container>
   );
 }
