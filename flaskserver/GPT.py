c47093f80133e1ae9c772a4da8ee7ed33186fab4
dragonman164 <sanidhiyafirefox123@gmail.com>
2023-12-27 13:14:01 +0530

    template for ocrmodel:

--- flaskserver/GPT.py
+++ flaskserver/GPT.py
@@ -9,19 +9,19 @@ def askGPT(prompt):
     }
 
     json_data = {
-        'model': 'text-davinci-003',
+        'model': '',
         'prompt': prompt,
         'temperature': 1,
-        'max_tokens': 256,
+        'max_tokens': 500, # Change this value to generate more text
         'top_p': 1,
         'frequency_penalty': 0,
         'presence_penalty': 0,
+        'model' : "gpt-3.5-turbo-instruct"
     }
 
     try: 
         response = requests.post('https://api.openai.com/v1/completions', headers=headers, json=json_data)
         response_data = json.loads(response.text)
-        print(response_data)
         return response_data['choices'][0]['text']
     except requests.exceptions.RequestException as e:
         raise Exception("Error while Processing GPT Response",str(e))
